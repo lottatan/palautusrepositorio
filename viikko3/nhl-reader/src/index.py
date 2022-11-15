7962a2a62e11f21af839dc412a9d2bff4d2a6ff4
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-11-15 13:40:08 +0200

    sortaus

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -5,19 +5,20 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    print("Players from FIN 2021-01-04 19:15:32.858661")
+    #print(response)
 
     players = []
 
     for player_dict in response:
         player = Player(
-            player_dict['name']
+            player_dict['name'], player_dict["team"], player_dict["goals"], player_dict["assists"], player_dict["nationality"]
         )
-
-        players.append(player)
+        if player.nationality == "FIN":
+            players.append(player)
 
     print("Oliot:")
+    players.sort(key=lambda x: x.summa, reverse=True)
 
     for player in players:
         print(player)
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+        self.summa = self.goals + self.assists
+        self.nationality = nationality
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team} {self.assists} + {self.goals} = {self.summa}"
