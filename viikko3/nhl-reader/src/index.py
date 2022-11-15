8a2dd572265cb2baa0f01a842518a37b0c24ef51
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-11-15 13:56:24 +0200

    uudet luokat

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,25 +1,14 @@
 import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     print("Players from FIN 2021-01-04 19:15:32.858661")
-    #print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict['name'], player_dict["team"], player_dict["goals"], player_dict["assists"], player_dict["nationality"]
-        )
-        if player.nationality == "FIN":
-            players.append(player)
-
-    print("Oliot:")
-    players.sort(key=lambda x: x.summa, reverse=True)
-
+    
     for player in players:
         print(player)
 
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, name, team, goals, assists, nationality):
         self.name = name
@@ -9,3 +11,26 @@ class Player:
     
     def __str__(self):
         return f"{self.name:20} {self.team} {self.assists} + {self.goals} = {self.summa}"
+
+class PlayerReader:
+    def __init__(self, url):
+        self.reader = requests.get(url).json()
+
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        players = []
+        for player_dict in self.reader.reader:
+            player = Player(
+                player_dict['name'], player_dict["team"], player_dict["goals"], player_dict["assists"], player_dict["nationality"]
+            )
+            if player.nationality == nationality:
+                players.append(player)
+
+        players.sort(key=lambda x: x.summa, reverse=True)
+
+        return players
